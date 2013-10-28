607c3efed5b33b28b72d0158e9a13e73babcd99c
xiaonan wang <xw535@nyu.edu>
2013-10-28 15:45:56 -0400

    first commit

---
+++ src/OurGame/Board.java
@@ -0,0 +1,5 @@
+package OurGame;
+
+public class Board {
+
+}
---
+++ src/OurGame/Dude.java
@@ -0,0 +1,11 @@
+package OurGame;
+
+import javax.swing.JFrame;
+
+public class Dude {
+
+  public static void mani (String[] args) {
+      
+    JFrame frame = new JFrame ("gameloft test");
+  }
+}
