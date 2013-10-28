afde2b6c4a88a32842ab44ab183ef021a7fbac47
xiaonanwang <xw535@nyu.edu>
2013-10-28 18:07:02 -0400

    commit

--- src/OurGame/Board.java
+++ src/OurGame/Board.java
@@ -14,8 +14,8 @@ public class Board extends JPanel implements ActionListener{
 	  p = new Dude();
 	  addKeyListener(new AL());
 	  setFocusable(true);
-	  ImageIcon i = new ImageIcon("/Users/xiaonanwang/Documents/workspace/GameBeforeTest/src/OurGame/testp.jpg");
-	  //ImageIcon i = new ImageIcon(this.getClass().getResource("tesstp.jpg"));
+	  //ImageIcon i = new ImageIcon("/Users/xiaonanwang/Documents/workspace/GameBeforeTest/src/OurGame/testp.jpg");
+	  ImageIcon i = new ImageIcon(this.getClass().getResource("testp.jpg"));
 	  img = i.getImage();
 	  time = new Timer(5,this);
 	  time.start();
