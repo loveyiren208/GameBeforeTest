53258dc52babb517fe2403be30bd445dbe06b0fd
xiaonanwang <xw535@nyu.edu>
2013-10-29 18:46:40 -0400

    new images

--- src/OurGame/Dude.java
+++ src/OurGame/Dude.java
@@ -6,54 +6,63 @@ import java.awt.event.KeyEvent;
 import javax.swing.ImageIcon;
 
 public class Dude {
-  int x,dx,y;
-  Image still;
-  public Dude() {
-	  ImageIcon i = new ImageIcon("/Users/xiaonanwang/Documents/workspace/GameBeforeTest/src/OurGame/still.png");
-	  still = i.getImage();
-	  x = 10;
-	  y = 172;
-	  
-  }
-  
-  public void move() {
-	  x = x + dx;
-  }
-  
-  public int getX(){
-	  return x;
-  }
-  
-  public int getY() {
-	  return y;
-  }
-  
-  public Image getImage() {
-	  return still;
-  }
-  
-  public void keyPressed (KeyEvent e) {
-	  int key = e.getKeyCode();
-	  
-	  if (key == KeyEvent.VK_LEFT) {
-		dx = -1;  
-	  }
-	  
-	  if (key == KeyEvent.VK_RIGHT) {
-		  dx = 1;
-	  }
-  }
-  
-  public void keyReleased (KeyEvent e) {
-	  int key = e.getKeyCode();
-	  
-	  if (key == KeyEvent.VK_LEFT) {
-		dx = 0;  
-	  }
-	  
-	  if (key == KeyEvent.VK_RIGHT) {
-		 dx = 0;;
-	  }
-  }
-  
+	int x, dx, y,nx2,nx;
+	Image still;
+
+	public Dude() {
+		ImageIcon i = new ImageIcon(this.getClass().getResource("still.png"));
+        ImageIcon l = new ImageIcon(this.getClass().getResource("left.png"));
+		// ImageIcon i = new
+		// ImageIcon("/Users/xiaonanwang/Documents/workspace/GameBeforeTest/src/OurGame/");
+		still = i.getImage();
+		x = 75;
+		nx2 = 685;
+		y = 172;
+		nx = 0;
+
+	}
+
+	public void move() {
+		x = x + dx;
+		nx2 = nx2 +dx;
+		nx = nx + dx;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public Image getImage() {
+		return still;
+	}
+
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+
+		if (key == KeyEvent.VK_LEFT) {
+			dx = -1;
+		}
+
+		if (key == KeyEvent.VK_RIGHT) {
+			dx = 1;
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {
+		int key = e.getKeyCode();
+
+		if (key == KeyEvent.VK_LEFT) {
+			dx = 0;
+		}
+
+		if (key == KeyEvent.VK_RIGHT) {
+			dx = 0;
+			;
+		}
+	}
+
 }
--- src/OurGame/Frame.java
+++ src/OurGame/Frame.java
@@ -3,12 +3,16 @@ package OurGame;
 import javax.swing.JFrame;
 
 public class Frame {
-	public static void main (String[] args) {
-	    
-	    JFrame frame = new JFrame ("gameloft before test");
+	public Frame() {
+		JFrame frame = new JFrame ("gameloft before test");
 	    frame.add(new Board());
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setSize(1200,365);
+	    frame.setSize(700,365);
 	    frame.setVisible(true);
+	}
+	
+	public static void main (String[] args) {
+	    new Frame();
+	    
 	  }
 }
